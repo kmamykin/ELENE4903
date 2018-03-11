c603e904f63799b5606e99fd86145314625a50b8
Kliment Mamykin <kmamyk@gmail.com>
2018-03-11 17:33:01 -0400

    Problem 1 done

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import pandas as pd
 from numpy.linalg import inv
+from IPython.display import display
+import matplotlib.pyplot as plt
 
 
 def load_data(data_dir):
@@ -11,6 +13,14 @@ def load_data(data_dir):
     y_train = pd.read_csv(os.path.join(data_dir, 'y_train.csv'), header=None)
     y_test = pd.read_csv(os.path.join(data_dir, 'y_test.csv'), header=None)
     return X_train.values, y_train.values, X_test.values, y_test.values
+
+
+def accuracy_metric(y_true, y_pred):
+    return np.sum(y_true == y_pred) / y_true.shape[0]
+
+
+def mrse_metric(y_pred, y_true):
+    return np.sqrt(np.sum((y_pred - y_true) ** 2) / y_true.shape[0])
 
 
 def kernel(kernel_fn, signature='(i),(i)->()'):
@@ -30,6 +40,7 @@ def kernel(kernel_fn, signature='(i),(i)->()'):
 
 def gaussian(b=1):
     return kernel(lambda x1, x2: np.exp((-1 / b) * np.sum((x1 - x2) ** 2)))
+
 
 class GaussianProcess(object):
 
@@ -55,5 +66,37 @@ class GaussianProcess(object):
         cov = self.covariance(x0)
         return np.sqrt(cov.diagonal()).flatten()
 
+
 def problem_1_part_a():
-    pass
+    X_train, y_train, X_test, y_test = load_data(data_dir='./hw3/hw3-data/gaussian_process')
+    gp = GaussianProcess(X_train, y_train, kernel=gaussian(b=10), sigma=0.01)
+    predictions = gp.mean(X_test)
+    print("Test MRSE: {:.2f}".format(mrse_metric(predictions, y_test)))
+
+
+def problem_1_part_b():
+    X_train, y_train, X_test, y_test = load_data(data_dir='./hw3/hw3-data/gaussian_process')
+    bs = [5, 7, 9, 11, 13, 15]
+    variances = [.1, .2, .3, .4, .5, .6, .7, .8, .9, 1]
+    msres = np.zeros([len(bs), len(variances)])
+    for b_index, b in enumerate(bs):
+        for v_index, v in enumerate(variances):
+            gp = GaussianProcess(X_train, y_train, kernel=gaussian(b=b),  sigma=np.sqrt(v))
+            predictions = gp.mean(X_test)
+            msres[b_index, v_index] = mrse_metric(predictions, y_test)
+
+    table = pd.DataFrame(msres, index=["b={}".format(b) for b in bs], columns=["s^2={}".format(v) for v in variances])
+    display(table)
+
+def problem_1_part_d():
+    X_train, y_train, X_test, y_test = load_data(data_dir='./hw3/hw3-data/gaussian_process')
+    X_one_dim = np.sort(X_train[:, 4].flatten()) # has to be sorted to make sense on the plot
+    gp = GaussianProcess(X_one_dim, y_train, kernel=gaussian(b=5),  sigma=np.sqrt(2))
+    predictions = gp.mean(X_one_dim).flatten()
+    plt.figure(figsize=(16, 3))
+    plt.scatter(X_one_dim, y_train[:,0], color='blue')
+    plt.scatter(X_test[:, 4], y_test[:,0], color='red')
+    plt.plot(X_one_dim, predictions, color='black')
+
+    # std = gp.std(X_one_dim)
+    # plt.fill_between(X_one_dim, predictions - 2 * std, predictions + 2 * std, alpha=0.2, color='k')
