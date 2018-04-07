b14293ceba4306bbbf411acb2489a7a70248d029
Kliment Mamykin <kmamyk@gmail.com>
2018-04-06 20:06:28 -0400

    Refactored hw4

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -54,16 +54,6 @@ def ridge_regression_solution(X, y):
     return np.dot(np.dot(np.linalg.inv(diag + np.dot(X.T, X)), X.T), y).flatten()
 
 
-def joined_log_likelihood(params, ratings, user_embeddings, movie_embeddings):
-    ii = ratings['user_id'].values
-    jj = ratings['movie_id'].values
-    M = ratings['rating'].values
-    log_p_data = (-1/(2*params['sigma']**2)) * np.sum((M - np.sum(user_embeddings[ii] * movie_embeddings[jj], axis=1))**2)
-    log_p_prior_users = (-1*params['lambda']/2.0) * np.sum(user_embeddings ** 2)
-    log_p_prior_movies = (-1*params['lambda']/2.0) * np.sum(movie_embeddings ** 2)
-    return log_p_data + log_p_prior_users + log_p_prior_movies
-
-
 def predict(rows, cols, user_embeddings, movie_embeddings):
     selected_users = user_embeddings[rows]
     selected_movies = movie_embeddings[cols]
@@ -72,44 +62,106 @@ def predict(rows, cols, user_embeddings, movie_embeddings):
 
 
 def rmse_metric(y_true, y_pred):
-    n = y_true.flatten().shape[0]
-    return np.sqrt(np.sum((y_pred.flatten() - y_true.flatten()) ** 2) / n)
+    return np.sqrt(np.average((y_pred.flatten() - y_true.flatten()) ** 2))
 
 
-def similar_movies(movie, movies, movie_embeddings):
-    movie_idx = movies.index(movie)
-    query_embedding = movie_embeddings[movie_idx].reshape((1, -1))
-    # Using Euclidean distance
-    # target embedding will be broadcasted to match dims
-    # print(movie_embeddings.shape, query_embedding.shape, (movie_embeddings * query_embedding).shape)
-    distances = np.sqrt(np.sum((movie_embeddings - query_embedding)**2, axis=1))
-    closest_idx = np.argsort(distances, axis=None)
-    # print('distances', distances.shape, distances[:5], distances[-5:])
-    # print('closest_idx', closest_idx[:5], closest_idx[-5:])
-    # print('close/far dist', distances[closest_idx[:5]], distances[closest_idx[-5:]])
-    return [movies[i] for i in closest_idx[1:11]] # index 0 is the movie (closest to itself), ignore it
+class SparseMatrixFactorization(object):
+
+    def __init__(self, ratings, movies, params):
+        self.ratings = ratings
+        self.movies = movies
+        self.params = params
+        d = self.params['d']
+        n_of_users = ratings.loc[:, 'user_id'].max() + 1
+        n_of_movies = ratings.loc[:, 'movie_id'].max() + 1
+        # both embeddings start random, so it does not matter from which we start coordinate ascend
+        self.user_embeddings = np.random.randn(n_of_users, d)
+        self.movie_embeddings = np.random.randn(n_of_movies, d) # this is a transpose of the shape in lectures, but more convenient for calculations
+
+    def update(self):
+        for i in range(self.user_embeddings.shape[0]):
+            features, targets = select_user_data(self.ratings, self.movie_embeddings, i)
+            self.user_embeddings[i] = ridge_regression_solution(features, targets)
+        for j in range(self.movie_embeddings.shape[0]):
+            features, targets = select_movie_data(self.ratings, self.user_embeddings, j)
+            self.movie_embeddings[j] = ridge_regression_solution(features, targets)
+        return self
+
+    def train(self, iterations=100):
+        history = np.zeros(iterations)
+        for iteration in range(iterations):
+            self.update()
+            log_p = self.log_likelihood()
+            history[iteration] = log_p
+        return history[-1], history
+
+    def log_likelihood(self):
+        ii = self.ratings['user_id'].values
+        jj = self.ratings['movie_id'].values
+        M = self.ratings['rating'].values
+        log_p_data = (-1 / (2 * self.params['sigma'] ** 2)) * np.sum((M - np.sum(self.user_embeddings[ii] * self.movie_embeddings[jj], axis=1)) ** 2)
+        log_p_prior_users = (-1 * self.params['lambda'] / 2.0) * np.sum(self.user_embeddings ** 2)
+        log_p_prior_movies = (-1 * self.params['lambda'] / 2.0) * np.sum(self.movie_embeddings ** 2)
+        return log_p_data + log_p_prior_users + log_p_prior_movies
+
+    def rmse(self, ratings_test):
+        predicted_ratings = predict(ratings_test['user_id'].values, ratings_test['movie_id'].values, self.user_embeddings, self.movie_embeddings)
+        return rmse_metric(ratings_test['rating'].values, predicted_ratings)
+
+    def similar_movies(self, movie, first=10):
+        movie_idx = self.movies.index(movie)
+        query_embedding = self.movie_embeddings[movie_idx].reshape((1, -1))
+        # Using Euclidean distance
+        # target embedding will be broadcasted to match dims
+        distances = np.sqrt(np.sum((self.movie_embeddings - query_embedding) ** 2, axis=1))
+        closest_idx = np.argsort(distances, axis=None)
+        return [self.movies[i] for i in closest_idx[1:first+1]]  # index 0 is the movie (closest to itself), ignore it
 
 
-if __name__ == '__main__':
-    movies, ratings, ratings_test = load_data('./hw4-data')
-    params = { 'sigma': 0.5, 'lambda': 1.0 }
-    user_embeddings, movie_embeddings = initialize_matrix_factorization(ratings, d=10)
-    for iteration in range(100):
-        for i in range(user_embeddings.shape[0]):
-            features, targets = select_user_data(ratings, movie_embeddings, i)
-            user_embeddings[i] = ridge_regression_solution(features, targets)
-        for j in range(movie_embeddings.shape[0]):
-            features, targets = select_movie_data(ratings, user_embeddings, j)
-            movie_embeddings[j] = ridge_regression_solution(features, targets)
-        log_p = joined_log_likelihood(params, ratings, user_embeddings, movie_embeddings)
-        predicted_ratings = predict(ratings_test['user_id'].values, ratings_test['movie_id'].values, user_embeddings, movie_embeddings)
-        rmse = rmse_metric(ratings_test['rating'].values, predicted_ratings)
-        print("Iteration: {}, log likelihood: {:.8f}, RMSE: {:.2f}".format(iteration, log_p, rmse))
+def factorize_ratings():
+    movies, ratings, ratings_test = load_data('./hw4/hw4-data')
+    params = { 'sigma': 0.5, 'lambda': 1.0, 'd': 10 }
+    curves = pd.DataFrame()
+    final_stats = pd.DataFrame(columns=['train objective', 'test RMSE'])
+    models = []
+    for run in range(10):
+        model = SparseMatrixFactorization(ratings, movies, params)
+        objective, training_history = model.train(iterations=100)
+        rmse = model.rmse(ratings_test)
+        curves[run] = pd.Series(training_history)
+        final_stats.loc[run] = [objective, rmse]
+        models.append(model)
 
+        print("run: {} completed. LL: {:.8f}, RMSE: {:.8f}".format(run, objective, rmse))
+    print("Best run: {}".format(final_stats['train objective'].argmax()))
+    best_model = models[final_stats['train objective'].argmax()]
+    return best_model, curves, final_stats.sort_values(by=['train objective'], ascending=False)
+
+
+def problem2_a1(model, curves, final_stats):
+    plt.figure(figsize=(16, 5))
+    for i in range(len(curves.columns)):
+        plt.plot(curves.iloc[1:, i], label='run ' + str(i))
+    plt.ylabel('Train objective')
+    plt.xlabel('Iteration')
+    plt.legend()
+
+
+def problem2_a2(model, curves, final_stats):
+    display(final_stats)
+
+
+def problem2_b(model, curves, final_stats):
     query_movies = [
         'Star Wars (1977)',
         'My Fair Lady (1964)',
         'GoodFellas (1990)'
     ]
+    df = pd.DataFrame()
     for movie in query_movies:
-        print("{}:{}".format(movie, similar_movies(movie, movies, movie_embeddings)))
+        df[movie] = pd.Series(model.similar_movies(movie, first=10))
+        # print("{}:{}".format(movie, model.similar_movies(movie, first=10)))
+    display(df)
+
+if __name__ == '__main__':
+    pass
