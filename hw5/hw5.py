0aa512fef52b7963e62d8be35f63aa9ee90bc113
Kliment Mamykin <kmamyk@gmail.com>
2018-04-28 16:38:29 -0400

    Markov Chain team ranking

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -2,7 +2,8 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy.linalg import inv, svd
+from numpy import linalg
+
 import pandas as pd
 from IPython.display import display
 
@@ -12,15 +13,15 @@ def read_scores_file(data_dir='./hw5/hw5-data'):
     with open(os.path.join(data_dir, 'TeamNames.txt'), encoding='utf-8') as f:
         lines = f.read()
     teams = [l for l in lines.strip().split('\n')]
-    return np.array(teams), scores
+    return teams, scores
 
 
 def construct_transition_matrix(scores):
     N_teams = max(scores.iloc[:,0].max(), scores.iloc[:,2].max())
     M = np.zeros((N_teams, N_teams), dtype=np.float)
     for row in scores.itertuples(index=False):
-        i, j = row[0] - 1, row[2] - 1 # 1 based indices to 0 based
-        points_i, points_j = row[1], row[3]
+        i, points_i, j, points_j = row
+        i, j = i - 1, j - 1 # 1 based indices to 0 based
         assert points_i != points_j, 'No ties allowed in the tournament!'
         team_a_win_bonus = 1. if points_i > points_j else 0
         team_b_win_bonus = 1. if points_i < points_j else 0
@@ -33,12 +34,52 @@ def construct_transition_matrix(scores):
 
 
 def stationary_distribution(M):
-    U, S, _ = svd(M.T)
-    return (U[0] / np.sum(U[0]))#.reshape((1, -1))
+    # linalg.eig is computing right eigenvectors
+    # state probabilities are multiplied from the left (probs @ M)
+    # So we need to transpose M before taking eigen decomposition
+    evals, evecs = linalg.eig(M.T)
+    max_eval_idx = np.argmax(evals) # eigenvalues are not sorted
+    eig_vec = np.real(evecs[:, max_eval_idx]).reshape((-1, 1)) # is a column vector
+    eig_val = np.real(evals[max_eval_idx])
+    sdist = (eig_vec / np.sum(eig_vec)).reshape((1, -1)) # is a row vector
+    assert np.allclose(M.T @ eig_vec, eig_val * eig_vec) # ensure proper eigen vector/value
+    assert np.allclose(sdist @ M, sdist) # ensure proper stationary distribution
+    return sdist
 
 
 def top_teams(teams, w, top=25):
-    indices = np.argsort(w)
+    flat_w = w.flatten()
+    indices = np.argsort(flat_w)
     indices = indices[::-1] # reverse
-    print(indices)
-    return teams[indices[:top]], w[indices[:top]]
+    top_indices = indices[:top]
+    return np.array(teams)[top_indices], flat_w[top_indices]
+
+
+def markov_chain_experiment():
+    teams, scores = read_scores_file(data_dir='./hw5/hw5-data')
+    M = construct_transition_matrix(scores)
+    N, _ = M.shape
+    w_infinity = stationary_distribution(M)
+    checkpoint_iterations = [10, 100, 1000, 10000]
+    max_iterations = checkpoint_iterations[-1]
+    w = np.ones((1, N)) / N # Initial uniform distribution
+    team_rankings = pd.DataFrame(columns=["t={}".format(i) for i in checkpoint_iterations])
+    norm_p1_trend = np.zeros(max_iterations)
+    for t in range(1, max_iterations+1):
+        w = w @ M
+        assert np.allclose(np.sum(w), 1.0)
+        norm_p1_trend[t-1] = linalg.norm(w - w_infinity, ord=1)
+        if t in checkpoint_iterations:
+            column = checkpoint_iterations.index(t)
+            team_rankings.iloc[:,column] = top_teams(teams, w, top=25)[0]
+
+    # print(top_teams(teams, w, top=25))
+    return dict(team_rankings=team_rankings, norm_p1_trend=norm_p1_trend)
+
+
+def problem_1_a(experiment):
+    display(experiment['team_rankings'])
+
+
+def problem_1_b(experiment):
+    plt.plot(experiment['norm_p1_trend'])
